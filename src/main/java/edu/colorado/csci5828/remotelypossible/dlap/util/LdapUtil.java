8d7cb0534b652448754d6ed31794387954fa5151
Andrew Grossnickle <TeraInferno@users.noreply.github.com>
2016-10-16 14:44:51 -0600

    Added git ignore file. Added ldap sdk. Added initial ldap util java file. Added local mvn repo for ldap sdk. Added utility mvn script.

---
+++ src/main/java/edu/colorado/csci5828/remotelypossible/dlap/util/LdapUtil.java
@@ -0,0 +1,17 @@
+package edu.colorado.csci5828.remotelypossible.dlap.util;
+
+import com.unboundid.ldap.sdk.LDAPConnection;
+
+public class LdapUtil {
+
+	LDAPConnection ldapConnectionl;
+	
+	public LdapUtil()
+	{
+		ldapConnectionl = new LDAPConnection();
+		
+	}
+	
+	
+	
+}
