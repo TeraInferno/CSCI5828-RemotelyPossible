56acfce3791c190d31c457927c49cb1aab41ca39
Brett Shouse <idahoincolorado@gmail.com>
2016-10-20 20:39:52 -0600

    Strip to base model for later build-up

--- src/main/java/edu/colorado/csci5828/remotelypossible/dlap/model/Application.java
+++ src/main/java/edu/colorado/csci5828/remotelypossible/dlap/model/Application.java
@@ -5,11 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="faculty")
+@Table(name="application")
 public class Application {
 
 	@Id
@@ -17,43 +16,6 @@ public class Application {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id = -1L;
 	
-	@Column(name="name", length=254)
-	private String name;
-	
-	@OneToOne
-	private PhoneNumber phone = new PhoneNumber();
-	
-	@Column(name="email", length=254)
-	private String email;
-	
-	@Column(name="program", length=8)
-	private String program;
-	
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public PhoneNumber getPhone() {
-		return phone;
-	}
-	public void setPhone(PhoneNumber phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getProgram() {
-		return program;
-	}
-	public void setProgram(String program) {
-		this.program = program;
-	}
 	public Long getId() {
 		return id;
 	}
