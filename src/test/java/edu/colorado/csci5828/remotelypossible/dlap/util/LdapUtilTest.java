d3062509c62a4461489b7b7717e3f9df90c10edd
Andrew Grossnickle <TeraInferno@users.noreply.github.com>
2016-10-29 14:10:03 -0600

    LDAP sample data plus unit test file

---
+++ src/test/java/edu/colorado/csci5828/remotelypossible/dlap/util/LdapUtilTest.java
@@ -0,0 +1,38 @@
+package edu.colorado.csci5828.remotelypossible.dlap.util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+
+public class LdapUtilTest {
+	
+	InMemoryDirectoryServer testServer;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAuthenticate() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
