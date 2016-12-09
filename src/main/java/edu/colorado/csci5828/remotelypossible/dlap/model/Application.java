eddd366a6278cdf57828f58340405bcb4cdac8a1
U-Alienz\Steve <stivymail@yahoo.com>
2016-12-09 01:20:02 -0700

    Add fields to model application for scoring function

--- src/main/java/edu/colorado/csci5828/remotelypossible/dlap/model/Application.java
+++ src/main/java/edu/colorado/csci5828/remotelypossible/dlap/model/Application.java
@@ -2,10 +2,14 @@ package edu.colorado.csci5828.remotelypossible.dlap.model;
 
 import javax.persistence.*;
 
+import edu.colorado.csci5828.remotelypossible.dlap.common.Constants;
+
 @Entity
 @Table(name="application")
 public class Application {
 
+    public static final int SCORE_DISQUALIFIED = -1;
+  
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +23,21 @@ public class Application {
     
     @OneToOne
     private ApprenticeshipInfo apprenticeshipInfo;
+        
+    @Column(length=256, nullable=true)
+    private String disqualReason = Constants.EMPTY_STRING;
+    
+    @Column(nullable=false)
+    private Boolean override = Boolean.FALSE;
+    
+    @OneToOne
+    private Project assigned;
+        
+    @Column(nullable=false)
+    private int score = 0;
+    
+    
+    
 
     public Long getId() {
       return id;
@@ -50,6 +69,38 @@ public class Application {
 
     public void setUsername(String username) {
       this.username = username;
+    }
+
+    public String getDisqualReason() {
+      return disqualReason;
+    }
+
+    public void setDisqualReason(String disqualReason) {
+      this.disqualReason = disqualReason;
+    }
+
+    public Boolean getOverride() {
+      return override;
+    }
+
+    public void setOverride(Boolean override) {
+      this.override = override;
+    }
+
+    public Project getAssigned() {
+      return assigned;
+    }
+
+    public void setAssigned(Project assigned) {
+      this.assigned = assigned;
+    }
+
+    public int getScore() {
+      return score;
+    }
+
+    public void setScore(int score) {
+      this.score = score;
     }
 
 
